9f2b9ddf4f4326fde7e09ee480119e396f8aeb06
Jayson Scruggs <jayson.scruggs@gmail.com>
2021-06-13 11:42:26 -0500

    Added leave-room api

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import GetRoom, RoomView, CreateRoomView, JoinRoom, UserInRoom
+from .views import GetRoom, LeaveRoom, RoomView, CreateRoomView, JoinRoom, UserInRoom
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path("get-room", GetRoom.as_view()),
     path("join-room", JoinRoom.as_view()),
     path("user-in-room", UserInRoom.as_view()),
+    path("leave-room", LeaveRoom.as_view()),
 ]
